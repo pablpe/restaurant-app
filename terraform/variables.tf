4cee86e00f401fc424e7bf0c833aec0b7a86cc1a
pablpe <pablo.perez.colls@gmail.com>
2024-10-04 11:03:48 +0200

    finished instances and sg -- missing reference to vpc and subnets

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  type    = string
+  default = "ami-08eb150f611ca277f"
+}
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+variable "key_name" {
+  type = string
+}
+
