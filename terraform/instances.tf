b288bc66c4365d0ff78b22403a124b7090aeeb9c
mpaserrano <miguel.paa.serrano@gmail.com>
2024-10-04 15:06:17 +0100

    Added s3 and dynamodb control, added outputs

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,59 +1,59 @@
 ## client
 resource "aws_instance" "client" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.public.id]
-    subnet_id = aws_subnet.private_subnet.id
-    tags = {
-        Name = "client ec2"
-    }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.public.id]
+  subnet_id       = aws_subnet.private_subnet.id
+  tags = {
+    Name = "client ec2"
+  }
 }
 
 ## auth
 resource "aws_instance" "auth" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.private.id]
-    subnet_id = aws_subnet.private_subnet.id
-    tags = {
-        Name = "auth ec2"
-    }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.private.id]
+  subnet_id       = aws_subnet.private_subnet.id
+  tags = {
+    Name = "auth ec2"
+  }
 }
 
 ## discounts
 resource "aws_instance" "discounts" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.private.id]
-    subnet_id = aws_subnet.private_subnet.id
-    tags = {
-        Name = "discounts ec2"
-    }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.private.id]
+  subnet_id       = aws_subnet.private_subnet.id
+  tags = {
+    Name = "discounts ec2"
+  }
 }
 
 ## items
 resource "aws_instance" "items" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.private.id]
-    subnet_id = aws_subnet.private_subnet.id
-    tags = {
-        Name = "items ec2"
-    }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.private.id]
+  subnet_id       = aws_subnet.private_subnet.id
+  tags = {
+    Name = "items ec2"
+  }
 }
 
 ## haproxy
 resource "aws_instance" "haproxy" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.public.id]
-    subnet_id = aws_subnet.public_subnet.id
-    tags = {
-        Name = "haproxy ec2"
-    }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.public.id]
+  subnet_id       = aws_subnet.public_subnet.id
+  tags = {
+    Name = "haproxy ec2"
+  }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "restaurant-app-state-bucket"
+    key            = "state/key"
+    region         = "eu-north-1"
+    dynamodb_table = "restaurant-table"
+  }
+}
+
