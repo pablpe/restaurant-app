b851e7aa8ada51498d3d650789b47fdddaad393d
mpaserrano <74569110+mpaserrano@users.noreply.github.com>
2024-10-14 16:23:32 +0100

    Update start.sh with comments

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 #directories
+# this gets the current directory for the script (it's not used, but it's here if we need it)
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# terraform folder
 terraform_dir="terraform"
+
+# ansioble folder
 ansible_dir="ansible"
 
+
+#Menu prints
 function menu(){
-    clear
+    clear #Clears the loop, so when you go back to the menu you always have a clean terminal
     echo "##### MAIN MENU #####"
     echo "1 - terraform"
     echo "2 - ansible"
@@ -35,35 +42,40 @@ function ansible_menu(){
     echo "0 - main menu"
 }
 
+
+#Functions that execute the options selected in the menu
 function terraform_function(){
 
+	#Default value, can be anything as long as it's not 0 (exit value), if it reaches 0 it will exit the loop and thus exiting the menu
     terraform_loop="9"
-    while [ "$terraform_loop" != "0" ]
+	
+	
+    while [ "$terraform_loop" != "0" ] #if it reaches 0 it leaves the loop
     do
-        terraform_menu
-        read user_choice
+        terraform_menu #Calls the terraform menu function
+        read user_choice #Reads user input (choice)
 
-        terraform_loop=$user_choice
+        terraform_loop=$user_choice #terraform_loop acquires the value of the user input to see if it's 0 for the loop validation
 
         echo -e "\n\n"
 
-        case $user_choice in
+        case $user_choice in #Selects what to execute based on user choices
         "1")
-            echo "Cleaning terraform directories"
+            echo "Cleaning terraform directories" #Prints what we're doing
 
-            cd "$terraform_dir"
+            cd "$terraform_dir" #Changes working directory to the terraform directory defined above
 
-            rm -rf .terraform .terraform.lock.hcl terraform.tfstate.backup
+            rm -rf .terraform .terraform.lock.hcl terraform.tfstate.backup #removes all terraform files, effectively cleaning the repo
 
-            echo -e "\n Cleaning done"
+            echo -e "\n Cleaning done" #Job done
 
-            echo -e "\n >>>> PRESS ANY KEY TO CONTINUE <<<<"
-            read continue
+            echo -e "\n >>>> PRESS ANY KEY TO CONTINUE <<<<" #Letting the user know he has to click anything to go back to menu
+            read continue #Reads user input, does not need validation as it can be any key (it isn't used as anything else, just for the user to recognize the job is done)
 
-            cd ..
+            cd .. #Changes the directory back
 
             ;;
-        "2")
+        "2") #It is mostly the same for the other options, so I will not comment further
             echo "Initiating terraform . . ."
 
             cd "$terraform_dir"
@@ -145,6 +157,7 @@ function terraform_function(){
     done
 }
 
+#Loop works the same as the previous one and the main menu one, so I will just comment what our menu options do
 function ansible_function(){
     ansible_value="9"
 
@@ -159,13 +172,14 @@ function ansible_function(){
 
         case $user_choice in
         "1")
-            echo "Cleaning inventory file"
-            >ansible/inventory
+            echo "Cleaning inventory file" #let us know what we're doing
+            >ansible/inventory #Wipes ansible inventory file
 
-            echo -e "\nGenerating ansible invetory . . . "
+            echo -e "\nGenerating ansible invetory . . . " # #Lets us know what we're doing
 
-            cd "$terraform_dir"
+            cd "$terraform_dir" #changes directory to terraform directory to get the outputs
 
+			#Getting the outputs
             haproxy_ip=$(terraform output -raw haproxy)
             frontend_ip=$(terraform output -raw frontend)
             auth_ip=$(terraform output -raw auth)
@@ -175,6 +189,7 @@ function ansible_function(){
             discount_loadbalancer_ip=$(terraform output -raw discount-loadbalancer)
             items_loadbalancer_ip=$(terraform output -raw items-loadbalancer)
 
+			#Pringint the outputs
             echo -e "\n Gathering IPs"
             echo "haproxy       : $haproxy_ip"
             echo "frontend      : $frontend_ip"
@@ -185,6 +200,9 @@ function ansible_function(){
             echo "loadbalancer - discount : $discount_loadbalancer_ip"
             echo "loadbalancer - items : $items_loadbalancer_ip"
 
+			#This needs to be indented all the way to the left or the EOF will not register it and it will break the code
+			#This is the ansible inventory file content built in this script, that will then be inserted into the inventory file
+			#If you want to change the inventory file, do it here
             echo -e "\n Generating ansible inventory file"
             inventory_content=$(cat <<-EOF
 [haproxy]
@@ -207,16 +225,16 @@ ansible_ssh_extra_args='-o StrictHostKeyChecking=no'
 EOF
             )
 
-            cd ..
+            cd .. #Changes directory to main folder
 
-            echo "$inventory_content" >> "$ansible_dir/inventory"
+            echo "$inventory_content" >> "$ansible_dir/inventory" #Inserts the inventory content above to the inventory file
 
-            echo -e "\n Inventory generated"
+            echo -e "\n Inventory generated" #Lets us know the job has been completed
 
             echo -e "\n >>>> PRESS ANY KEY TO CONTINUE <<<<"
             read continue
             ;;
-        "2")
+        "2") #The other options just execute commands
             echo -e "Pinging hosts"
 
             cd "$ansible_dir"
@@ -231,7 +249,7 @@ EOF
             echo -e "Running setup playbook"
 
             cd "$ansible_dir"
-            ansible-playbook setup.yml
+            ansible-playbook setup.yml #you can change the playbook here, should be a variable but I could not be bothered
 
             cd ..
 
@@ -245,29 +263,34 @@ EOF
     done
 }
 
+#Main menu
+#How it works?
+#it creates a loop where the menu is always printed at the beginning
+
+#Default value, can be anything as long as it's not 0 (exit value), if it reaches 0 it will exit the loop and thus exiting the menu
 loop_value="9"
 
-while [ "$loop_value" != "0" ]
+while [ "$loop_value" != "0" ] #if it reaches 0 it leaves the loop
 do
-    menu
+    menu #Prints the main menu
 
-    read user_reply
+    read user_reply #Reads user input (choice)
     
-    loop_value=$user_reply
+    loop_value=$user_reply #loop_value acquires the value of the user input to see if it's 0 for the loop validation
 
-    echo -e "\n\n"          
+    echo -e "\n\n"        #Just spacing, -e means it can read \n which is basically an Enter
 
-    case $user_reply in
+    case $user_reply in  #Switch case to examine user's choices
     "1")
-        terraform_function
+        terraform_function #Calls the terraform function which contains the terraform submenu and the actual commands
         ;;
     "2")
-        ansible_function
+        ansible_function #Calls the ansible function which contains the ansible submenu and the actual commands
         ;;
     "0")
-        echo -e "\n\n Exiting . . ."
+        echo -e "\n\n Exiting . . ." #Just printing exiting..., -e means it can read \n which is basically an Enter
         ;;
     esac
 done    
 
-echo "exited"
+echo "exited" #Informs the user he has left the loop
